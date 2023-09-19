9516c52051c8098ad52b1e8dcc30f600b1c5042a
yohanlopezc <75345853+yohanlopezc@users.noreply.github.com>
2023-09-19 16:03:29 -0500

    1 binaries uploaded

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,7 @@
-import 'zone.js/dist/zone';
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { bootstrapApplication } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-@Component({
-  selector: 'my-app',
-  standalone: true,
-  imports: [CommonModule],
-  template: `
-    <h1>Hello from {{ name }}!</h1>
-    <a target="_blank" href="https://angular.io/start">
-      Learn more about Angular
-    </a>
-  `,
-})
-export class App {
-  name = 'Angular';
-}
+import { AppModule } from './app/app.module';
 
-bootstrapApplication(App);
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));

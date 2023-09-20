67bf215b25a2108808f11643cbc23c79ed5e5459
yohanlopezc <75345853+yohanlopezc@users.noreply.github.com>
2023-09-19 22:49:49 -0500

    Agregar una ruta detalle del héroe

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 imports: [ RouterModule.forRoot(routes) ],
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 exports: [ RouterModule ]
 { path: 'dashboard', component: DashboardComponent },

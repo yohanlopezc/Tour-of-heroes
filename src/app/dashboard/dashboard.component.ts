e926b12f988f9342b965c5efe06c6c0f4756376b
yohanlopezc <75345853+yohanlopezc@users.noreply.github.com>
2023-09-19 22:55:02 -0500

    HeroDetailComponent enrutable

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',

50822226e2b6e20076db9151324f6834dedebf03
yohanlopezc <75345853+yohanlopezc@users.noreply.github.com>
2023-09-19 22:13:35 -0500

    Observable HeroService

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',

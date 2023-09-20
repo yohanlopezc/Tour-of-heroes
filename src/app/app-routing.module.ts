b776fdb93dba7faea14f3c0111f59dc414a49146
yohanlopezc <75345853+yohanlopezc@users.noreply.github.com>
2023-09-19 22:48:14 -0500

    Agrega una ruta al arreglo AppRoutingModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,3 +2,4 @@ imports: [ RouterModule.forRoot(routes) ],
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 exports: [ RouterModule ]
+{ path: 'dashboard', component: DashboardComponent },

45991e7d495ca8f33ebb7c9248ce8f9bccfeb8f6
yohanlopezc <75345853+yohanlopezc@users.noreply.github.com>
2023-09-19 22:11:07 -0500

    reeemplazando  la definición de la propiedad heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -10,7 +11,7 @@ import { HEROES } from '../mock-heroes';
 
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
 
   constructor() { }

e4a23d1f55c51cb305ec066c74cc9a251332b0af
yohanlopezc <75345853+yohanlopezc@users.noreply.github.com>
2023-09-19 22:12:08 -0500

    get heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -23,4 +23,9 @@ export class HeroesComponent implements OnInit {
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
+  
 }
